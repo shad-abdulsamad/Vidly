7cc03726b53aca8b90600096949ec03598212a8f
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-02-22 21:40:45 +0300

    hashing passwords

---
+++ hash.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcrypt');
+
+async function run(){
+   
+    console.log(hashed);
+
+}
+
+run();
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -13,7 +14,8 @@ router.post('/', async(req,res)=>{
   if(user) return res.status(400).send('User already Exists.');
 
   user = new User(_pick(user, ['name', 'email', 'password']));
-
+   const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
   res.send( _.pick(user, ['_id', 'name', 'email']));
