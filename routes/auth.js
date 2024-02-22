34494739cf3aa7ba0bb1fdbb35c4c2df88457cff
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-02-22 22:32:08 +0300

    authenticating users

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const {User} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/', async(req,res)=>{
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({email:req.body.email});
+  if(!user) return res.status(400).send('Invalid email or password');
+
+  const validPassword = bcrypt.compare(req.body.password, user.password);
+  if(!validPassword) return res.status(400).send('Invalid email or password');
+
+  res.send(true);
+});
+
+function validate(user) {
+  const schema = {
+    email: Joi.string().min(5).max(255).email().required(),
+    password: Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+module.exports = router
